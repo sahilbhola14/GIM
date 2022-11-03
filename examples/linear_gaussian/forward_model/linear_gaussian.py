090dea97ee5be8ae9cfdf2a79d7b2f5fbda62c5d
sahilbhola14 <sbhola@umich.edu>
2022-11-03 01:23:26 -0400

    (refactor) Add forward model heirarchy gaussian

---
+++ examples/linear_gaussian/forward_model/linear_gaussian.py
@@ -0,0 +1,54 @@
+import numpy as np
+import os
+import shutil
+import matplotlib.pyplot as plt
+
+class linear_gaussian():
+    def __init__(self, true_theta=np.arange(1,4), spatial_res=100):
+        self.spatial_res = spatial_res
+        self.xtrain = np.linspace(-1, 1, self.spatial_res)
+        self.true_theta = true_theta
+        self.num_parameters = self.true_theta.shape[0]
+        self.vm = self.compute_vm()
+
+    def compute_vm(self):
+        vm = np.tile(self.xtrain[:, None], (1, self.num_parameters))
+        vm = np.cumprod(vm, axis=1)
+        return vm
+
+    def compute_prediction(self, theta):
+        return self.vm@theta.reshape(-1, 1)
+
+
+def main():
+    # User input
+    true_theta = np.arange(3)
+    spatial_res = 100
+    # End User input
+    model = linear_gaussian(true_theta=true_theta, spatial_res=spatial_res)
+    prediction = model.compute_prediction(theta=true_theta)
+
+    data = np.zeros((spatial_res, 2))
+    data[:, 0] = model.xtrain
+    data[:, 1] = prediction.ravel()
+
+    # Create the folders
+    save_path = "./Output"
+
+    if os.path.exists(save_path) is True:
+        shutil.rmtree(save_path)
+
+    os.mkdir(save_path)
+
+    data_save_path = os.path.join(save_path, "true_output.npy")
+    np.save(data_save_path, data)
+
+
+
+
+    
+
+if __name__=="__main__":
+    main()
+    
+
