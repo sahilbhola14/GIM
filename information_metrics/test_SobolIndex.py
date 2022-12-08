484f6403f3999f62215f53a54f8bd65a4da7e1e7
sahilbhola14 <sbhola@umich.edu>
2022-12-08 15:31:57 -0500

    feat(Sobol Index) Add Total sobol index test

--- information_metrics/test_SobolIndex.py
+++ information_metrics/test_SobolIndex.py
@@ -14,15 +14,16 @@ def comp_sobol_indices():
         forward_model=forward_model,
         prior_mean=np.ones(3).reshape(-1, 1),
         prior_cov=np.eye(3),
-        global_num_outer_samples=4800,
-        global_num_inner_samples=4800,
-        model_noise_cov_scalar=1e-1,
+        global_num_outer_samples=1200,
+        global_num_inner_samples=1200,
+        model_noise_cov_scalar=0,
         data_shape=(1, num_samples),
         write_log_file=True,
         save_path="./Output/SobolIndex",
     )
 
-    sobol_index.comp_sobol_indices()
+    sobol_index.comp_first_order_sobol_indices()
+    sobol_index.comp_total_effect_sobol_indices()
 
 
 def comp_linear_gaussian_prediciton(num_samples, theta):
@@ -30,10 +31,10 @@ def comp_linear_gaussian_prediciton(num_samples, theta):
     x = np.linspace(0, 1, num_samples)
 
     # Unequal contribution of the parameters
-    # A = np.vander(x, len(theta), increasing=True)
+    A = np.vander(x, len(theta), increasing=True)
 
     # Equal contribution of each parameter
-    A = np.ones((x.shape[0], len(theta)))
+    # A = np.ones((x.shape[0], len(theta)))
     return np.dot(A, theta).reshape(1, -1)
 
 
